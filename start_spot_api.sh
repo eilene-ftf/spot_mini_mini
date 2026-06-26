6375d4b406616813f1ca0c9be5fb07f95e7bf3a1
Eilene <eileneftf@gmail.com>
2026-06-26 17:53:59 -0400

    added option to start with or without client from cmd line

--- start_spot_api.sh
+++ start_spot_api.sh
@@ -2,5 +2,9 @@
 
 cd spot_bullet/src
 uv run API_server_d.py &
-uv run API_client_d.py &
+echo "============================================================================================="
+if [ $1 -eq 1 ]; then
+  echo "============================================================================================="
+  uv run API_client_d.py &
+fi
 uv run env_tester_controller.py -dr
