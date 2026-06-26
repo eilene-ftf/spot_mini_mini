a69ff310bd9f57bfa590cad9aa0a0ea0e1c1572c
Eilene <eileneftf@gmail.com>
2026-06-26 17:58:04 -0400

    added error message

--- start_spot_api.sh
+++ start_spot_api.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-cd spot_bullet/src
-uv run API_server_d.py &
-echo "============================================================================================="
-if [ $1 -eq 1 ]; then
+if [ $# -lt 1 ]; then
+  echo "Intended use: ./start_spot_api.sh <0/1> where 0 indicates starting without client and 1 indicates starting without"
+else
+  cd spot_bullet/src
+  uv run API_server_d.py &
   echo "============================================================================================="
-  uv run API_client_d.py &
+  if [ $1 -eq 1 ]; then
+    echo "============================================================================================="
+    uv run API_client_d.py &
+  fi
+  uv run env_tester_controller.py -dr
 fi
-uv run env_tester_controller.py -dr
