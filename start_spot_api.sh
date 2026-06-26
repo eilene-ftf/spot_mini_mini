08480dc1136e10e011f3b3ad5e323165f79361ae
Eilene <eileneftf@gmail.com>
2026-06-26 18:08:00 -0400

    added client-only mode

--- start_spot_api.sh
+++ start_spot_api.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-  echo "Intended use: ./start_spot_api.sh <0/1> where 0 indicates starting without client and 1 indicates starting without"
+  echo "Intended use: ./start_spot_api.sh <0/1/2> where 0 indicates starting without client and 1 indicates starting with; 2 indicates client only, not running the sim"
+elif [ $1 -eq 2 ]; then
+  echo "Starting client"
+  cd spot_bullet/src
+  uv run API_client_d.py
 else
+  echo "Starting server"
   cd spot_bullet/src
   uv run API_server_d.py &
-  echo "============================================================================================="
   if [ $1 -eq 1 ]; then
-    echo "============================================================================================="
+    echo "Starting client"
     uv run API_client_d.py &
   fi
+  echo "Starting sim"
+  echo "============================================================================================="
   uv run env_tester_controller.py -dr
 fi
